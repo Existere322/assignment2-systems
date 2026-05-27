b77fe321b58c9f53931dac2fbc379155a094e5d8
cantwakeup <s2874774504@gmail.com>
2026-05-27 19:23:46 +0800

    update_profiling

---
+++ cs336_systems/benchmarking_script.py
@@ -0,0 +1,158 @@
+from cs336_basics.model import BasicsTransformerLM
+from cs336_basics.optimizer import get_cosine_lr, AdamW
+import timeit
+import argparse
+import torch
+import numpy as np
+import random
+import os
+import json
+
+def parse_args():
+    p = argparse.ArgumentParser()
+    g_model = p.add_argument_group("model")
+    g_model.add_argument("--vocab_size", type=int, default=10000)
+    g_model.add_argument("--context_length", type=int, default=256)
+    g_model.add_argument("--d_model", type=int, default=512)
+    g_model.add_argument("--batch_size", type=int, default=64)
+    g_model.add_argument("--num_layers", type=int, default=4)
+    g_model.add_argument("--num_heads", type=int, default=16)
+    g_model.add_argument("--d_ff", type=int, default=1344)
+    g_model.add_argument("--rope_theta", type=float, default=10000.0)
+    
+    g_optimizer = p.add_argument_group("optimizer")
+    g_optimizer.add_argument("--weight_decay", type=float, default=0.1)
+    g_optimizer.add_argument("--betas", type=float, default=[0.9, 0.95], nargs=2)
+    g_optimizer.add_argument("--eps", type=float, default=1e-8)
+
+    g_training = p.add_argument_group("training")
+    g_training.add_argument("--max_learning_rate", type=float, default=1e-3)
+    g_training.add_argument("--min_learning_rate", type=float, default=1e-4)
+    g_training.add_argument("--warmup_iters", type=int, default=1000)
+    g_training.add_argument("--cosine_cycle_iters", type=int, default=19500)
+    g_training.add_argument("--end_iter", type=int, default=20000)
+    g_training.add_argument("--grad_clip_norm", type=float, default=1.0)
+    g_training.add_argument("--val_interval", type=int, default=500)
+    g_training.add_argument("--val_batches", type=int, default=50)
+    g_training.add_argument("--device", type=str, default="cuda")
+    g_training.add_argument("--seed", type=int, default=336)
+    g_training.add_argument("--dtype", type=str, default="float32",
+                        choices=["float32", "float16", "bfloat16"])
+
+    g_profiling = p.add_argument_group("profiling")
+    g_profiling.add_argument("--warmup_steps", type=int, default=5)
+    g_profiling.add_argument("--profiling_steps", type=int, default=10)
+    g_profiling.add_argument("--profiling_warmup", type=int, default=0)
+
+    return p.parse_args()
+    
+
+def generate_random_data(batch_size, context_length, vocab_size, device):
+    data = torch.randint(low=0, high=vocab_size,
+                        size=(batch_size, context_length + 1),
+                        device=device, 
+                        dtype=torch.long)
+    # pytorch 中索引要求为 int64 因此 dtype 使用 torch.long
+
+    x = data[:, :-1]
+    y = data[:, 1:]
+
+    return x, y
+
+
+def cross_entropy(inputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+
+    batch_size = inputs.shape[0]
+    max_entry = torch.max(inputs, dim=-1, keepdim=True)
+    inputs_submax = inputs - max_entry.values
+    inputs_exp = torch.exp(inputs_submax)
+    inputs_sum = torch.sum(inputs_exp, dim=-1)
+    result = torch.log(inputs_sum) - inputs_submax[torch.arange(batch_size, device=inputs.device), targets]
+    # -log(exp(target) / sum_exp(i)) = log(sum_exp(i)) - target
+    # 如果 inputs_sum 选择了保留维度，那么相减的时候，就是 batch_size, 1 与 batch_size 进行相减
+    # 会被 pytorch 广播为 batch_size, batch_size 和 batch_size, batch_size 
+    # 前者在第二个维度的数字被广播了 batch_size 遍，后者也是每个数字广播这么多
+    # 因此做差后结果是 batch_size, batch_size 但是第二个维度上每个向量内容都一样
+    # 取平均值，最后又变成了 所有内容相加除以总数，对于向量内部 n 个内容相加又除以 n 因此不变，最后结果一样
+    # 但是却增大了计算量
+    result = result.mean()
+
+    return result
+
+    
+def main(args):
+    vocab_size = args.vocab_size
+    context_length = args.context_length
+    d_model = args.d_model
+    batch_size = args.batch_size
+    num_layers = args.num_layers
+    num_heads = args.num_heads
+    d_ff = args.d_ff
+    rope_theta = args.rope_theta
+    weight_decay = args.weight_decay
+    [beta1, beta2] = args.betas
+    eps =  args.eps
+    max_learning_rate = args.max_learning_rate
+    device = args.device
+    torch.manual_seed(args.seed)
+    if  torch.cuda.is_available():
+        torch.cuda.manual_seed(args.seed)
+        torch.cuda.manual_seed_all(args.seed)
+    np.random.seed(args.seed)
+    random.seed(args.seed)
+    warmup_steps = args.warmup_steps
+    profiling_steps = args.profiling_steps
+
+    transformer_model = BasicsTransformerLM(vocab_size,
+                                        context_length,
+                                        d_model, num_layers,
+                                        num_heads, d_ff,
+                                        rope_theta)
+    transformer_model.to(device)
+    
+    optimizer = AdamW(transformer_model.parameters(), args.max_learning_rate)
+
+    log_file = open(os.path.join(os.path.dirname(__file__), "profiling_results/profiling.jsonl"), "a")
+    log_file.write(json.dumps({**vars(args)}) + "\n")
+    log_file.flush()
+
+
+    for t in range(0, args.end_iter):
+        step = t + 1
+        lr = get_cosine_lr(step, max_learning_rate, args.min_learning_rate, args.warmup_iters, args.cosine_cycle_iters)
+        for g in optimizer.param_groups:
+            g["lr"] = lr
+        x, y = generate_random_data(batch_size, context_length, vocab_size, device)
+
+        time_start = timeit.default_timer()
+        logits = transformer_model(x)
+        loss = cross_entropy(logits.view(-1, logits.size(-1)), y.reshape(-1))
+        # view(-1, logits.size(-1)) 前一个 -1 表示由 pytorch 自行推断维度，最后一个表示最后一维度的数量
+        # 将 batch_size, seq_len, vocab_size 的结果转变为 batch_size * seq_len, vocab_size 的维度
+        torch.cuda.synchronize()  
+        time_after_forward = timeit.default_timer()
+        forward_time = time_after_forward - time_start
+        
+        loss.backward()
+        torch.cuda.synchronize()  
+        time_after_backward = timeit.default_timer()
+        backward_time = time_after_backward - time_after_forward
+
+        optimizer.step()
+        torch.cuda.synchronize()  
+        time_after_step = timeit.default_timer()
+        step_time = time_after_step - time_after_backward
+        optimizer.zero_grad()
+        torch.cuda.synchronize()  
+        # Wait for all kernels in all streams on a CUDA device to complete.
+        # In this case, the time we measured is the time of forward plus backward and optimizer update
+        if step > warmup_steps + profiling_steps:
+            exit(0)
+
+        if step > warmup_steps or args.profiling_warmup == 1:
+            log_file.write(json.dumps({"step": step, "forward_time": forward_time, 
+                                       "backward_time": backward_time, "step_time": step_time}) + "\n")
+            log_file.flush()
+
+
+if __name__ == "__main__": main(parse_args())
